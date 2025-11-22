ebf27a0768153889b58e36a3e1dea424b3c61fd5
Ali Zoubair <ali.zoubair@outlook.com>
2025-11-23 00:20:18 +0100

    Add root Terraform versions.tf configuration

---
+++ infrastructure/terraform/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 0.8"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
+
